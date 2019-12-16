0e33c066eed1ba30decb7d475103d38760056b63
RaviTeja, Lakumarapu (Cognizant) <RaviTeja.Lakumarapu@cognizant.com>
2019-12-16 10:21:42 +0530

    iiht

---
+++ micro-services/training_microservice/src/main/java/com/cognizant/trainingmicroservice/repository/TrainingRepository.java
@@ -0,0 +1,21 @@
+package com.cognizant.trainingmicroservice.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.cognizant.trainingmicroservice.entity.Mentor;
+import com.cognizant.trainingmicroservice.entity.Training;
+import com.cognizant.trainingmicroservice.entity.User;
+
+public interface TrainingRepository extends JpaRepository<Training, Long> {
+
+	List<Training> findAllByMentor(Mentor mentor);
+
+	List<Training> findAllByUser(User user);
+	
+	 
+  
+
+}
